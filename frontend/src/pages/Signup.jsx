8a667c63ab2f10513816215132ad425325078363
Anupama <anupamahera2022@gmail.com>
2024-05-19 23:20:01 +0530

    created signup page UI

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../css/signup.css'
+import {Link} from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='signup'>
+      <h1>Sign up</h1>
+      <form className='form'>
+      <input type="text" placeholder='username'
+      className='text1' id='username'/>
+      <input type="email" placeholder='email'
+      className='text2' id='email'/>
+      <input type="password" placeholder='password'
+      className='text3' id='password'/>
+      <button className='sign-up-btn'>Sign up</button>
+     
+      <div className="account">
+        <p>Have an account ?  <Link to={'/sign-in'} className='link'>Sign-in</Link></p>
+      </div>
+     
+      </form>
+    
+    </div>
   )
 }
 
