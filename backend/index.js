c6a918a29e30542ef1fe8ce312b258e5fb3907d7
Anupama <anupamahera2022@gmail.com>
2024-05-19 12:50:42 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+//config dotenv
+dotenv.config();
 
 //create express app
 const app = express();
 
 
+mongoose.connect(process.env.MONGO)
+        .then(()=>{
+            console.log('Connected to MongoDB');
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
 //creating and starting server
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
 })
+
+//mongodb+srv://root:pass@mern-estate.7vjcm6l.mongodb.net/?retryWrites=true&w=majority&appName=Mern-Estate
