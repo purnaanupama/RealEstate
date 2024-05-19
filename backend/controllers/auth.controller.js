6a73d73c6400b81590446317d9d49f1f63a2ce64
Anupama <anupamahera2022@gmail.com>
2024-05-19 15:47:10 +0530

    create middleware to handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const User = require('../models/user.model');
 const bcryptjs= require('bcryptjs');
+const errorHandler = require('../utils/error')
 
-exports.register = async(req,res)=>{
+exports.register = async(req,res,next)=>{
   const {username,email,password,role} = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({username, email, password : hashedPassword})
@@ -13,6 +14,6 @@ exports.register = async(req,res)=>{
       data : req.body
     })
   }catch(error){
-     res.status(500).json(error);
+     next(error)
   }
-}
+};
