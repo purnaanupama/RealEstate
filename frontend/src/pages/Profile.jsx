3a16eed296536fc344e6ca327f2ec946c98f9fb4
Anupama <anupamahera2022@gmail.com>
2024-05-28 22:29:01 +0530

    completed update user frontend and backend connection

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,21 +4,27 @@ import { useSelector } from 'react-redux'
 import { useRef, useState, useEffect } from 'react'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../../firebase.js'
+import { updateUserStart, updateUserSuccess, updateUserFailure} from '../redux/userSlice.jsx';
+import { useDispatch } from 'react-redux';
 
 const Profile = () => {
-  const { currentUser } = useSelector((state) => state.user)
+  const { currentUser,loading,error } = useSelector((state) => state.user)
   const fileRef = useRef(null)
   const [filePerc, setFilePerc] = useState(0)
   const [file, setFile] = useState(undefined)
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({})
   const [isVisible, setIsVisible] = useState(false)
+  const [updateSuccess,setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
+ useEffect(() => {
     if (file) {
       handleFileUpload(file)
     }
   }, [file])
+
+
 
   useEffect(() => {
     if (filePerc === 100) {
@@ -33,6 +39,16 @@ const Profile = () => {
       return () => clearTimeout(timer)
     }
   }, [filePerc])
+
+  useEffect(()=>{
+    if(updateSuccess){
+      const timer = setTimeout(() => {
+        setUpdateSuccess(false);
+      }, 1500)
+
+      return () => clearTimeout(timer)
+    }
+  })
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app)
@@ -63,11 +79,43 @@ const Profile = () => {
     setFile(e.target.files[0])
   }
 
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+     });
+  }
+  const handleSubmit=async(e)=>{
+   e.preventDefault();
+    try {
+     dispatch(updateUserStart());
+     const res = await fetch(`/api/user/update/${currentUser._id}`,{
+      method:'POST',
+      headers : {
+       'Content-Type':'application/json'
+     },
+      body:JSON.stringify(formData)
+     })
+    
+     const data = await res.json();
+     console.log(data);
+     if(data.status === 'fail'){
+       dispatch(updateUserFailure(data.message));
+       return;
+     }
+     dispatch(updateUserSuccess(data))
+     setUpdateSuccess(true);
+    } catch (error) {
+     dispatch(updateUserFailure(error.message));
+     console.log(error);
+    }
+   }
+
   return (
     <div className='profile'>
       <h1>User Profile</h1>
 
-      <form className='form1'>
+      <form onSubmit={handleSubmit} className='form1' >
         <input
           onChange={handleFileChange}
           type='file'
@@ -94,18 +142,20 @@ const Profile = () => {
             ''
           )}
         </p>
-        <h3>{currentUser.role.toUpperCase()}</h3>
-        <input type='text' placeholder='username' className='text2' id='username' />
-        <input type='email' placeholder='email' className='text3' id='email' />
-        <input type='password' placeholder='password' className='text4' id='password' />
-        <button type='button' className='update'>
-          Update
+        <h3>{currentUser?currentUser.role.toUpperCase():''}</h3>
+        <input type='text' placeholder='username' defaultValue={currentUser.username} className='text2' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='email' className='text3' id='email' defaultValue={currentUser.email} onChange={handleChange}/>
+        <input type='password' placeholder='password' className='text4' id='password' onChange={handleChange}/>
+        <button type='submit' className='update'>
+         {loading?'Loading...':'Update'}
         </button>
       </form>
       <div className='options'>
         <p>Delete Account</p>
         <p>Sign Out</p>
       </div>
+      <p style={{color:'red',marginTop:'20px'}}>{error ? error: '' }</p>
+      <p style={{color:'green',marginTop:'20px'}}>{updateSuccess ? 'Updated Successfully': '' }</p>
     </div>
   )
 }
