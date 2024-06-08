bdf5a3455f707994a8c47416b207064bf8171159
Anupama <anupamahera2022@gmail.com>
2024-06-09 03:14:13 +0530

    create listing api route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const listingRouter = require('./routes/listing.route')
 const userRouter = require('./routes/user.route')
 const authRouter = require('./routes/auth.route')
 const cookieParser = require('cookie-parser');
@@ -35,6 +36,7 @@ app.listen(process.env.PORT || 3000,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use('/api/listing',listingRouter)
 app.use((err,req,res,next)=>{
    const statusCode = err.statusCode || 500;
    const message = err.message || 'Internal Server Error'
