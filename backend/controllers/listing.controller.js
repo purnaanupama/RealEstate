89d403c3a14a34d713dfce5a0963099ef67751e1
Anupama <anupamahera2022@gmail.com>
2024-07-10 17:07:18 +0530

    Finish listing filter backend

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -57,4 +57,50 @@ exports.getListing=async(req,res,next)=>{
         next(error)
     }
 }
+exports.getListings=async(req,res,next)=>{
+    try {
+      //if there is a limit use it and parse it otherwise send 9 listings to the page
+      const limit = parseInt(req.query.limit) || 9;
+      const startIndex = parseInt(req.query.startIndex) || 0;
 
+      let offers = req.query.offers;
+      if(offers === undefined||offers === 'false'){
+        offers={$in:[false,true]}
+      }
+
+      let furnished = req.query.furnished;
+      if(furnished === undefined||furnished === 'false'){
+        furnished = {$in:[false,true]}
+      }
+
+      let parking = req.query.parking;
+      if(parking === undefined||parking === 'false'){
+        parking = {$in:[false,true]}
+      }
+
+      let type = req.query.type;
+      if(type === undefined||type === 'all'){
+        type = {$in:['sale','rent']}
+      }
+
+      const searchItem = req.query.searchItem || '';
+      const sort = req.query.sort || 'createdAt';
+      const order = req.query.order || 'desc'
+
+      const listings = await Listing.find({
+        name:{$regex:searchItem,$options:'i'},
+        offers,
+        furnished,
+        parking,
+        type
+      }).sort(
+        {[sort]:order}
+      ).limit(limit).skip(startIndex)
+      
+      //return the filtered listings to the user
+      return res.status(200).json(listings)
+
+    } catch (error) {
+        next(error)
+    }
+}
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -7,5 +7,6 @@ router.post('/create',verifyToken,listingController.createListing);
 router.delete('/delete/:id',verifyToken,listingController.deleteListing);
 router.post('/update/:id',verifyToken,listingController.updateListing);
 router.get('/get/:id',listingController.getListing);
+router.get('/get',listingController.getListings);
 
 module.exports =  router;
